f3803f57273f3608358a230cc8ec41361e42042b
Colton-D-Carter <64115378+Colton-D-Carter@users.noreply.github.com>
2020-04-26 00:53:07 -0400

    fixed bugs

---
+++ Course-Portal-Home-Page/java.js
@@ -0,0 +1,5 @@
+
+const option = {weekday: 'log', day: 'numeric', month: 'long', year:'numeric'}
+document.grtElementByID(update).textContent = new Date().toLocaleDateString('en-US', 'option');
+
+"/Casper Sato/"
