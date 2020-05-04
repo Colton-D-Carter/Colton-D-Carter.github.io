b1cc833d13d2c45c320ab9496e017ae56a1a1e06
Colton-D-Carter <64115378+Colton-D-Carter@users.noreply.github.com>
2020-05-04 00:40:03 -0400

    Add files via upload
    5/4/2020 Still having problems with VS so this is just a temporary post

--- Course-Portal-Home-Page/java.js
+++ Course-Portal-Home-Page/java.js
@@ -2,4 +2,5 @@
 const option = {weekday: 'log', day: 'numeric', month: 'long', year:'numeric'}
 document.grtElementByID(update).textContent = new Date().toLocaleDateString('en-US', 'option');
 
+
 "/Casper Sato/"
