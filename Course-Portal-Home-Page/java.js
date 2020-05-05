145a23cf3374087ae18fee34ceff03b526996ee9
Colton-D-Carter <ccoltond@byui.edu>
2020-05-05 16:42:08 -0400

    vsfix1

--- Course-Portal-Home-Page/java.js
+++
@@ -1,6 +0,0 @@
-
-const option = {weekday: 'log', day: 'numeric', month: 'long', year:'numeric'}
-document.grtElementByID(update).textContent = new Date().toLocaleDateString('en-US', 'option');
-
-
-"/Casper Sato/"
---
+++ java.js
@@ -0,0 +1,6 @@
+
+const option = {weekday: 'log', day: 'numeric', month: 'long', year:'numeric'}
+document.grtElementByID(update).textContent = new Date().toLocaleDateString('en-US', 'option');
+
+
+"/Casper Sato/"
