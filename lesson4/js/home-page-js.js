fd8e55baf440154fe7248e0bc1c9c4eaf6a61aae
Colton-D-Carter <ccoltond@byui.edu>
2020-05-16 00:30:05 -0400

    base-page-for-town-home-page

---
+++ lesson4/js/home-page-js.js
@@ -0,0 +1,5 @@
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('.navmen');
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle
+('responsive')}, false);
