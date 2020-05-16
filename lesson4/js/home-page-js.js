fbf0c233519e9b92dd00fec8f6f243c32798876a
Colton-D-Carter <ccoltond@byui.edu>
2020-05-16 01:15:57 -0400

    date-js-ts1

--- lesson4/js/home-page-js.js
+++ lesson4/js/home-page-js.js
@@ -1,5 +1,17 @@
-alert ('I suck at JS 5!');
+alert ('I suck at JS 6!');
 const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('.ham');
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')});
+
+let w_name = ['Sunday', 'Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday'];
+let m_name = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let mydate = new Date();
+
+let weekname = w_name[mydate.getDay()];
+let monthname =m_name[mydate.getMonth()];
+
+const dateoutput = Document.querySelector('.date');
+
+dateoutput.textContent = weekname + ", " + mydate.getDate() + " " + monthname + " " + mydate.getFullYear();
